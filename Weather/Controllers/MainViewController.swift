2d9e82cb39c144d5bd6699ca58a6063be568c378
Nikita <semennikovna@yandex.ru>
2023-12-09 12:00:14 +0600

    Inital comit

---
+++ Weather/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Weather
+//
+//  Created by Nikita on 09.12.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ Weather/Views/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  Weather
+//
+//  Created by Nikita on 09.12.2023.
+//
+
+import UIKit
+
+class MainView: UIView {
+    
+    
+    //MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
